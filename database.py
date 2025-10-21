7e717cca9688cc5d1150ce75f726ec01759497b8
esther <estherdominion2002@gmail.com>
2025-10-21 13:32:34 +0100

    password

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine,text
 from  sqlalchemy.orm import sessionmaker
 from  dotenv import load_dotenv
+from pymysql.constants import CLIENT
 import os
 load_dotenv()
 
 
 db_url=f"mysql+pymysql://{os.getenv('dbuser')}:{os.getenv('dbpassword')}@{os.getenv('dbhost')}:{os.getenv('dbport')}/{os.getenv('dbname')}"
+
+
 
 
 # print("User:", os.getenv("dbuser"))
@@ -14,13 +17,41 @@ db_url=f"mysql+pymysql://{os.getenv('dbuser')}:{os.getenv('dbpassword')}@{os.get
 # print("Port:", os.getenv("dbport"))
 # print("DB Name:", os.getenv("dbname"))
 
-engine = create_engine(db_url)
+engine = create_engine(
+    db_url,
+connect_args={"client_flag": CLIENT.MULTI_STATEMENTS}
+)
 
 Session = sessionmaker(bind=engine)
 
 db= Session()
 
-query=text("select * from user")
+create_tables_query =text("""
+create table if not EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+    );
+                          
+create table if not EXISTS courses(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    TITLE VARCHAR(100) NOT NULL,
+    level VARCHAR(100) NOT NULL 
+    );
+create table if not EXISTS enrollments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    courseId INT,
+    FOREIGN KEY(userId) REFERENCES users(id),
+    FOREIGN KEY(courseId) REFERENCES courses(id)
+      );
+""")
+db.execute(create_tables_query)
+print("Table has been  created successfully")
 
-users=db=db.execute(query).fetchall()
-print(users)
+
+# query=text("select * from user")
+
+# users=db=db.execute(query).fetchall()
+# print(users)
---
+++ user.py
@@ -0,0 +1,95 @@
+# from database import db
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel,Field
+# from sqlalchemy import text
+# import os
+# from dotenv import load_dotenv
+# import bcrypt
+# import uvicorn
+
+# load_dotenv()
+# app = FastAPI(title="simple App", version="1.0.0")
+# class simple(BaseModel):
+#     name : str = Field(...,json_schema_extra={"examples": ["sam larry"]})
+#     email: str = Field(...,json_schema_extra={ "examples":["sam@gmail.com"])
+#     password: str = Field(...,examples="sam123")
+# @app.post("/signup")
+# def signup(input:simple):
+#     try:
+#         duplicate_query=text("""
+#             SELECT * FROM users
+#             WHERE email = :email
+
+#                             """)
+#         existing = db.execute(duplicate_query,{"email": input.email})
+#         if existing:
+#             raise HTTPException(status_code=400, detail="email already exists")
+#         query = text("""
+#             INSERT INTO users (name, email, password)
+#             VALUES (:name, :email, :password)
+#         """)
+#         salt = bcrypt.gensalt()
+#         hashedpassword = bcrypt.hashpw(input.password.encode('utf-0'), salt)
+#         print(hashedpassword)
+#         db.execute(query,{"name": input.name, "email": input.email, "password": hashedpassword})
+#         db.commit()
+#         return{"message":"user created successfully",
+#                "data":{"name": input.name, "email": input.email}}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=e)
+    
+
+#     if __name__=="__main__":
+#      uvicorn.run(app,host=os.getenv("host"), port=int(os.getenv("port")))
+
+
+from database import db
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy import text
+import os
+from dotenv import load_dotenv
+import bcrypt
+import uvicorn
+load_dotenv()
+app = FastAPI(title="Simple App", version="1.0.0")
+class Simple(BaseModel):
+    name: str = Field(..., example="Sam Larry")
+    email: str = Field(..., example="sam@email.com")
+    password: str = Field(..., example="sam123")
+@app.post("/signup")
+def signUp(input: Simple):
+    try:
+        duplicate_query=text("""
+            SELECT * FROM users
+            WHERE email = :email
+                             """)
+        existing = db.execute(duplicate_query, {"email": input.email})
+        if existing:
+            print("Email already exists")
+            # raise HTTPException(status_code=400, detail="Email already exists")
+        query = text("""
+            INSERT INTO users (name, email, password)
+            VALUES (:name, :email, :password)
+        """)
+        salt = bcrypt.gensalt()
+        hashedPassword = bcrypt.hashpw(input.password.encode('utf-8'), salt)
+        print(hashedPassword)
+        db.execute(query, {"name": input.name, "email": input.email, "password": hashedPassword})
+        db.commit()
+        return {"message": "User created successfully",
+                "data": {"name": input.name, "email": input.email}}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail = e)
+if __name__=="__main__":
+    uvicorn.run(app,host=os.getenv("host"), port=int(os.getenv("port")))
+
+
+
+
+
+
+
+
+
+
